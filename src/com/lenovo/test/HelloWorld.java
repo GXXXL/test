84de7c8bf714b78c520033e3a1fcd9372b331d66
GXXXL <475891342@qq.com>
2017-11-28 15:27:05 +0800

    The third project.

--- src/com/lenovo/test/HelloWorld.java
+++ src/com/lenovo/test/HelloWorld.java
@@ -2,9 +2,10 @@ package com.lenovo.test;
 
 public class HelloWorld {
     public static void main (String[] args ){
-        if (true){
+        int a = 1;
+        for (; a < 4; a++) {
 
         }
-        System.out.println("hello");
+        System.out.println(a + ":   hello");
     }
 }
